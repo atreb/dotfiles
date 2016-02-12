5a160feafa9399bc809b2dccfeb8df45969b7024
atreb <bhupendra.atre@dowjones.com>
2016-02-12 01:07:13 -0500

    added gitconfig

--- setup.sh
+++ setup.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+#-----------------------------------------------------
+# Following installations is assumed:
+# - wget vim git
+# Following files should exist:
+# - /Users/atreb/DEV/certificates/decoded.crt
+#-----------------------------------------------------
+
 function backup {
   BACKUP_DIR=~/BACKUP/`date +%s`
   mkdir -p $BACKUP_DIR
+  #git backup
+  [[ -e ~/.gitconfig ]] && mv ~/.gitconfig $BACKUP_DIR/gitconfig
   #vim backup
   [[ -d ~/.vim ]] &&  mv ~/.vim $BACKUP_DIR/vim
   [[ -e ~/.vimrc ]] && mv ~/.vimrc $BACKUP_DIR/vimrc
 }
 
 function install {
+  #git
+  ln -s "`pwd`/gitconfig" ~/.gitconfig
   #vim
   mkdir -p ~/.vim/_backup ~/.vim/_tmp ~/.vim/bundle ~/.vim/colors
   ln -s "`pwd`/vimrc" ~/.vimrc
